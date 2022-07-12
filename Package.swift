6602cafe3c40557e87ede752cf84051db7769bbe
Naresh Kommana's <naresh.kommana@onmobile.com>
2022-07-12 09:41:47 +0530

    Update

--- Package.swift
+++ Package.swift
@@ -4,30 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "OnMobileRBT",
+    name: "OnMobileRBTSDK",
+    platforms: [.iOS(.v10)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "OnMobileRBT",
-            targets: ["OnMobileRBT"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "OnMobileRBTSDK",
+            targets: ["OnMobileRBTSDK_VIL"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "OnMobileRBT",
-            dependencies: []),
+        .binaryTarget(
+            name: "OnMobileRBTSDK_VIL",
+            url: "https://github.com/ONMO/OnMobileRBT/releases/download/v3.1.1/OnMobileRBTSDK_VIL.xcframework.zip",
+            checksum: "bbf8197bf9466b5490ac959dfb9d9341b7d7f96ed5ff5fe0fd26d4428eec4f3e"
+        ),
 //        .binaryTarget(
-//                    name: "OnMobileRBTSDK",
-//                    url: "https://github.com/ONMO/OnMobileRBT/releases/download/v3.1.1/OnMobileRBTSDK-v3.1.1.zip",
-//                    checksum: "53ae076eaa2439281c1b81d01b3cc15395fb73a089f8b2c5186a86005a11cd70"
-//                ),
-//        .testTarget(
-//            name: "OnMobileRBTTests",
-//            dependencies: ["OnMobileRBT"]),
+//            name: "OnMobileRBTSDK_VIL",
+//            path: "Build/iOS/OnMobileRBTSDK_VIL.xcframework"
+//        ),
     ]
 )
