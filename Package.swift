011c0df211bf52c12266fdd9f2f8449659d4f5a6
Naresh Babu Kommana <naresh.kommana@onmobile.com>
2023-06-08 16:28:21 +0530

    [Update] : Package with ROBI

--- Package.swift
+++ Package.swift
@@ -10,12 +10,20 @@ let package = Package(
         .library(
             name: "OnMobileRBTSDK_VIL",
             targets: ["OnMobileRBTSDK_VIL"]),
+        .library(
+            name: "OnMobileRBTSDK_ROBI",
+            targets: ["OnMobileRBTSDK_ROBI"]),
     ],
     targets: [
         .binaryTarget(
             name: "OnMobileRBTSDK_VIL",
-            url: "https://github.com/ONMO/OnMobileRBT/releases/download/v3.1.5/OnMobileRBTSDK_VIL.xcframework.zip",
-            checksum: "9bb569b12b0bd22b36774702b0a5bb715b0e5041c0444a01ee4183ace1219458"
+            url: "https://github.com/ONMO/OnMobileRBT/releases/download/v3.1.6/OnMobileRBTSDK_VIL.xcframework.zip",
+            checksum: "40541c01f32396ddc2c5938c612678498f449a58a45f1f15b8bcf9ce5dc22015"
+        ),
+        .binaryTarget(
+            name: "OnMobileRBTSDK_ROBI",
+            url: "https://github.com/ONMO/OnMobileRBT/releases/download/v3.1.6/OnMobileRBTSDK_ROBI.xcframework.zip",
+            checksum: "2cd7986f0cbcb79873d2ce7d2fe4cc7183f065fd14a5403a5db49a65ee015e4f"
         ),
     ]
 )
